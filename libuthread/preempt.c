2cb0d15536e28dbc358fd34991a62e07f1f2bdfa
ZhengyuWu0924 <zywwu@ucdavis.edu>
2020-11-10 23:39:32 -0800

    Update preempt.c
    updated partial preemp functions

--- libuthread/preempt.c
+++ libuthread/preempt.c
@@ -15,6 +15,20 @@
  */
 #define HZ 100
 
+void handler(int signum){
+	// handling somethin
+	switch(signum){
+		case SIGALRM:
+			printf("sigalrm\n");
+			break;
+		case SIGVTALRM:
+			printf("sigvtalrm\n");
+			break;
+	}
+	return;
+} 
+
+
 void preempt_disable(void)
 {
 	/* TODO Phase 4 */
@@ -24,12 +38,24 @@ void preempt_enable(void)
 {
 	/* TODO Phase 4 */
 }
-
+/*
+ * preempt_start - Start thread preemption
+ *
+ * Configure a timer that must fire a virtual alarm at a frequency of 100 Hz and
+ * setup a timer handler that forcefully yields the currently running thread.
+ */
 void preempt_start(void)
 {
 	/* TODO Phase 4 */
-}
+	signal(SIGVTALRM, handler);
 
+}
+/*
+ * preempt_stop - Stop thread preemption
+ *
+ * Restore previous timer configuration, and previous action associated to
+ * virtual alarm signals.
+ */
 void preempt_stop(void)
 {
 	/* TODO Phase 4 */
