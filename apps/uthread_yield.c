fcdc9512cadd8413a1f8062ec04bcb570ebd3a88
ZhengyuWu0924 <zywwu@ucdavis.edu>
2020-11-12 16:09:51 -0800

    Update uthread_yield.c
    for prempt test

--- apps/uthread_yield.c
+++ apps/uthread_yield.c
@@ -1,4 +1,26 @@
 /*
+ * Simple hello world test
+ *
+ * Tests the creation of a single thread and its successful return.
+ */
+
+// #include <stdio.h>
+// #include <stdlib.h>
+
+// #include <uthread.h>
+
+// void hello(void *arg)
+// {
+// 	printf("Hello world!\n");
+// }
+
+// int main(void)
+// {
+// 	uthread_start(hello, NULL);
+
+// 	return 0;
+// }
+/*
  * Thread creation and yielding test
  *
  * Tests the creation of multiples threads and the fact that a parent thread
@@ -10,34 +32,68 @@
  * thread3
  */
 
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <assert.h>
+// #include <unistd.h>
+// #include <uthread.h>
+
+// void Preempt_test1(void *arg)
+// {
+// 	sleep(1);
+//     printf("Print 1st\n");
+//     return;
+// }
+
+// void Preempt_test2(void *arg)
+// {
+//     uthread_create(Preempt_test1, NULL);
+// 	printf("Print 2nd\n");
+//     // uthread_join(tid, &retval);
+// 	return;
+// }
+
+// void preempt_test(void)
+// {
+//     // uthread_join(uthread_create(Preempt_test2, NULL), &retval);
+// 	uthread_start(Preempt_test2,NULL);
+// }
+
+// int main(void)
+// {
+// 	preempt_test();
+// 	return 0;
+// }
+
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <assert.h>
+#include <unistd.h>
 #include <uthread.h>
 
-void thread3(void *arg)
+void Preempt_test1(void *arg)
 {
-	uthread_yield();
-	printf("thread3\n");
+	sleep(10);
+    printf("Print 1st\n");
+    return;
 }
 
-void thread2(void *arg)
+void Preempt_test2(void *arg)
 {
-	uthread_create(thread3, NULL);
-	uthread_yield();
-	printf("thread2\n");
+    uthread_create(Preempt_test1, NULL);
+
+    printf("Print 2nd\n");
+    uthread_yield();
+    return;
 }
 
-void thread1(void *arg)
+void preempt_test(void)
 {
-	uthread_create(thread2, NULL);
-	uthread_yield();
-	printf("thread1\n");
-	uthread_yield();
+    uthread_start(Preempt_test2,NULL);
 }
 
 int main(void)
 {
-	uthread_start(thread1, NULL);
+	preempt_test();
 	return 0;
 }
